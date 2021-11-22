80fe5247048416394dc354d11e1962f3887e32d2
Ryan McClue <re.mcclue@protonmail.com>
2021-11-22 16:25:53 +1100

    Add iterative implementation

--- code/closest_value_bst.py
+++ code/closest_value_bst.py
@@ -2,25 +2,24 @@
 # SPDX-License-Identifier: zlib-acknowledgement
 
 class Node:
-  def __init__(self, val, left, right):
-    self.val = val
+  def __init__(self, val, left = None, right = None):
+    self.value = value
     self.left = left
     self.right = right
 
 def solution(bst, target):
   cur_node = bst
-  prev_val, cur_val = 0, 0
-  while True:
-    cur_val = cur_node.val
+  closest = cur_node.value
 
-    if target == cur_val:
-      return cur_val
+  while cur_node is not None:
+    if abs(target - cur_node.value) < abs(target - closest):
+      closest = cur_node.value
 
-    if target > current_val:
-      cur_node = cur_node.right
-
-    if target < current_val:
+    if target < cur_node.value:
       cur_node = cur_node.left
+    elif target > cur_node.value:
+      cur_node = cur_node.right
+    else:
+      break
 
-    if not cur_node:
-      return current_val
+  return closest
