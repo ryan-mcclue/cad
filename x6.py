fbea1570b1c45e13eb2eab79192bf8ad89cc9d41
Ryan McClue <re.mcclue@protonmail.com>
2024-05-20 09:12:26 +1000

    k-most

--- x6.py
+++ x6.py
@@ -14,4 +14,36 @@ def perfect_number(n):
 # if next factor equals prev factor pair, gone far enough
 # n ** 0.5
 # pair_sum = reduce(lambda x, y: x + y, num)
-print(perfect_number(28))
+#print(perfect_number(28))
+
+# could construct a max heap from histogram in O(n)
+# then do a heap pop k times, k*logn
+def kmost(nums, k):
+  hist_map = {}
+  for i in nums:
+    key = str(i)
+    if key not in hist_map:
+      hist_map[key] = 1
+    else:
+      hist_map[key] += 1
+
+  ksort = [[0, 0]] * k
+  for num,freq in hist_map.items():
+    for i in range(k):
+      if freq > ksort[i][1]:
+        if i == k - 1:
+          print(f'setting {num}')
+          ksort[i][0] = num
+          ksort[i][1] = freq
+        else:
+          for j in range(k - 1, i, -1):
+            ksort[j][0] = ksort[j - 1][0]
+            ksort[j][1] = ksort[j - 1][1]
+          print(f'setting {num}')
+          ksort[i][0] = num
+          ksort[i][1] = freq
+        break
+
+  print(ksort)
+
+kmost([10, 20, 10, 30, 40], 2)
