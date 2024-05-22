350db5e39da64adbb9eb1df3ed49faffbc1d5848
Ryan McClue <re.mcclue@protonmail.com>
2024-05-23 09:57:59 +1000

    rotation

--- rotation.py
+++ rotation.py
@@ -1,4 +1,19 @@
 #!/usr/bin/python3
 # SPDX-License-Identifier: zlib-acknowledgement
-# Handmade Hero Day 092 - Filling Rotated and Scaled Rectangles
 
+def rotate_copy(a):
+  a_w = len(a[0])
+  a_h = len(a)
+  b_h = a_w
+  b_w = a_h
+  b = [[0] * b_w for i in range(b_h)]
+
+  # first row becomes last column
+  for y in range(a_h):
+    for x in range(a_w):
+      b[x][b_w - y - 1] = a[y][x]
+
+
+  print(b)
+
+rotate_copy([[1,2],[3,4],[5,6]])
