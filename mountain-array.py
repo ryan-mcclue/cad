38f59c33118a2e94bc3a861951d1ad14a351766b
Ryan McClue <re.mcclue@protonmail.com>
2024-05-30 08:49:17 +1000

    mountain

---
+++ mountain-array.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+# SPDX-License-Identifier: zlib-acknowledgement
+
+def m(a):
+  if len(a) < 3:
+    return False
+
+  are_increasing = True
+  for i in range(len(a)):
+    c = a[i]
+    
+    if are_increasing:
+      if i + 1 == len(a):
+        return False
+      else:
+        n = a[i + 1]
+        if n <= c:
+          are_increasing = False
+    else:
+      p = a[i - 1]
+      if p <= c:
+        return False
+
+  return True
+    
+
+print(m([2, 1]))
+print(m([3, 5, 5]))
