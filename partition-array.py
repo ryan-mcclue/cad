666795ccacc4f1f983b1f4132571198cfca5bfc9
Ryan McClue <re.mcclue@protonmail.com>
2024-06-03 08:54:05 +1000

    u

---
+++ partition-array.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# SPDX-License-Identifier: zlib-acknowledgement
+
+def p(arr):
+  # all values:
+  # for i in range(len):
+  #  for j in range(i, len):
+  target_s = sum(arr) // 3
