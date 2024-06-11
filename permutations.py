cdc439e8661f1e42c10bc7ad9330a0249ceea4e5
Ryan McClue <re.mcclue@protonmail.com>
2024-06-12 09:48:54 +1000

    j

--- permutations.py
+++ permutations.py
@@ -1,5 +1,22 @@
 #!/usr/bin/python3
 # SPDX-License-Identifier: zlib-acknowledgement
 def p(a):
-  for i in range(a):
-    a
+  remaining = a
+  res = []
+  cur = []
+  generate_permutations(cur, remaining, res)
+  return res
+
+# n! lists complexity
+def generate_permutations(cur, remaining, res):
+  if len(remaining) == 0:
+    res.append(cur)
+  else:
+    for num in remaining:
+      next_cur = cur.copy()
+      next_cur.append(num)
+      next_remaining = remaining.copy()
+      next_remaining.remove(num)
+      generate_permutations(next_cur, next_remaining, res)
+
+print(p([1,2,3]))
